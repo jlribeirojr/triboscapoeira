2ddfd3dc9d4cf17a9225dab56ab3d2461d859009
jlribeirojr <jlribeirojr@gmail.com>
2025-03-11 22:50:37 -0300

    Versão inicial do site Tribos Capoeira

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: [],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
